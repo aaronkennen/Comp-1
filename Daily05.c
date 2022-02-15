95749998823a310b69ecd8e49fd6fb0324eb1537
Aaron Kennen <79381363+aaronkennen@users.noreply.github.com>
2022-02-15 11:34:57 -0500

    Create Daily05.c

---
+++ Daily05.c
@@ -0,0 +1,56 @@
+/***********************************************
+Author: Aaron Kennen
+Date: 02/05/2021
+Purpose: <Insert the purpose of the program here>
+Sources of Help: PDF and my brain
+Time Spent: <Insert how much time you spent
+on the assignment here>
+***********************************************/
+
+/***********************************************
+Computing I -- COMP.1010 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order in the classroom,
+providing an enriching learning experience for students, and as training as a practicing
+computing professional upon graduation. This practice is manifested in the University's
+Academic Integrity policy. Students are expected to strictly avoid academic dishonesty and
+adhere to the Academic Integrity policy as outlined in the course catalog. Violations will be
+dealt with as outlined therein.
+All programming assignments in this class are to be done by the student alone. No outside help is
+permitted except the instructor and approved tutors.
+I certify that the work submitted with this assignment is mine and was generated in a manner
+consistent with this document, the course academic policy on the course website on Blackboard,
+and the UMass Lowell academic code.
+Date: 02/05/2021
+Name: Aaron Kennen
+***********************************************/
+/*
+#include <stdio.h>
+int main(int argc, char * argv[]){
+    
+    // Variables declared
+    int num1 = 100;
+    int num2 = 10;
+    int num3 = 16;
+    int num4 = 4;
+    
+    // Variable assignments are printed
+    printf("The first number is: %d\n", num1);
+    printf("The second number is: %d\n", num2);
+    printf("The third number is: %d\n", num3);
+    printf("The last number is: %d\n\n", num4);
+    
+    // Calculations of new variables
+    int sum = num1 + num2;
+    int product = num1 * num3;
+    int quotient = num2 / num4;
+    int remainder = num1 % num4;
+    
+    // New variables printed
+    printf("The sum of 100 and 10 is: %d\n", sum);
+    printf("The product of 100 and 16 is: %d\n", product);
+    printf("The quotient of 10 divided by 4 is: %d\n", quotient);
+    printf("The remainder of 100 divided by 4 is: %d\n", remainder);
+    
+    return 0;
+}
+*/
