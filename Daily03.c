57e2a68c78d866458a42d758183a923ea5b18f4a
Aaron Kennen <79381363+aaronkennen@users.noreply.github.com>
2022-02-15 11:32:48 -0500

    Create Daily03.c

---
+++ Daily03.c
@@ -0,0 +1,62 @@
+/***********************************************
+Author: Aaron Kennen
+Date: 02/03/2021
+Purpose: To show how to use Printf to print out a smiley face, while using \\ to print a backslash
+Sources of Help: The PDF File and my brain
+Time Spent: About 10-15 min, took longer to get it alligned than to start
+Question 1: You will need 18 printf functions for 3 smiley faces.
+Question 2: Yes a lot of the printf statements are repeats as you need to print more than one face
+Question 3: Only thing i could think of is put one smiley face in a function and then call it 3 times
+***********************************************/
+
+/*
+ Computing I -- COMP.1010 Honor Statement
+ 
+ The practice of good ethical behavior is essential for maintaining
+ good order in the classroom, providing an enriching learning
+ experience for students, and as training as a practicing computing
+ professional upon graduation.  This practice is manifested in the
+ University’s Academic Integrity policy. Students are expected to
+ strictly avoid academic dishonesty and adhere to the Academic
+ Integrity policy as outlined in the course catalog.  Violations
+ will be dealt with as outlined therein.
+ 
+ All programming assignments in this class are to be done by the
+ student alone.  No outside help is permitted except the instructor
+ and approved tutors.
+ 
+ I certify that the work submitted with this assignment is mine and
+ was generated in a manner consistent with this document, the
+ course academic policy on the course website on Blackboard, and
+ the UMass Lowell academic code.
+ 
+ Date: 02/03/2021
+ Name: Aaron Kennen
+ */
+ /*
+ #include <stdio.h>
+ 
+ int main(int argc, char * argv[]){
+    printf("  ****\n");
+    printf(" * . .*\n");
+    printf("*      *\n");
+    printf("*  \\_/ *\n");
+    printf(" *    *\n");
+    printf("  ****\n\n");
+    
+    printf("  ****\n");
+    printf(" * . .*\n");
+    printf("*      *\n");
+    printf("*  \\_/ *\n");
+    printf(" *    *\n");
+    printf("  ****\n\n");
+    
+    printf("  ****\n");
+    printf(" * . .*\n");
+    printf("*      *\n");
+    printf("*  \\_/ *\n");
+    printf(" *    *\n");
+    printf("  ****\n");
+    return 0;
+ }
+*/
