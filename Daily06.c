068323c43c27d4d26276f01966e8d699a3820aeb
Aaron Kennen <79381363+aaronkennen@users.noreply.github.com>
2022-02-15 11:36:04 -0500

    Create Daily06.c

---
+++ Daily06.c
@@ -0,0 +1,47 @@
+/***********************************************
+Author: Aaron Kennen
+Date: 02/08/2021
+Purpose: Use scanf to take user input and use if-else to determine if odd or even, and then complete an equation
+Sources of Help: PDF and my brain
+Time Spent: About 10-15 min
+***********************************************/
+
+/***********************************************
+Computing I -- COMP.1010 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order in the classroom,
+providing an enriching learning experience for students, and as training as a practicing
+computing professional upon graduation. This practice is manifested in the University's
+Academic Integrity policy. Students are expected to strictly avoid academic dishonesty and
+adhere to the Academic Integrity policy as outlined in the course catalog. Violations will be
+dealt with as outlined therein.
+All programming assignments in this class are to be done by the student alone. No outside help is
+permitted except the instructor and approved tutors.
+I certify that the work submitted with this assignment is mine and was generated in a manner
+consistent with this document, the course academic policy on the course website on Blackboard,
+and the UMass Lowell academic code.
+Date: 02/08/2021
+Name: Aaron Kennen
+***********************************************/
+/*
+#include <stdio.h>
+int main(int argc, char * argv[]){
+    
+    // Declaration
+    int number;
+    
+    // Display message to enter int, and accept with scanf
+    printf("Please enter a positive integer: ");
+    scanf("%d", &number);
+    
+    // If-Else to determin if odd or even
+    if(number %2 == 0){
+        number /= 2;
+        printf("The new value of the number is %d\n", number);
+    }else{
+        number = (number * 3) + 1;
+        printf("The new value of the number is %d\n", number);
+    }
+    
+    return 0;
+}
+*/
