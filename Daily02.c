899a1edd94a27bf52abce1662d910134ef3cbe8d
Aaron Kennen <79381363+aaronkennen@users.noreply.github.com>
2022-02-15 11:31:03 -0500

    Create Daily02.c

---
+++ Daily02.c
@@ -0,0 +1,53 @@
+/*
+  Author: Aaron Kennen
+  
+  Date: 01/30/2021
+  
+  Purpose: The purpose is to show what the executable path would be for the program, incase you need to run command line
+           arguments. Also it shows the functionality of printf and %s as well as seeing that argv[0] is ./a.out
+  
+  Sources of Help: The Day2.pdf and my brain
+  
+  Time Spent: About 10 minutes, took more time copying the code and writing this than understanding what it does.
+ */
+
+/*
+ Computing I -- COMP.1010 Honor Statement
+ 
+ The practice of good ethical behavior is essential for maintaining
+ good order in the classroom, providing an enriching learning
+ experience for students, and as training as a practicing computing
+ professional upon graduation.  This practice is manifested in the
+ University’s Academic Integrity policy. Students are expected to
+ strictly avoid academic dishonesty and adhere to the Academic
+ Integrity policy as outlined in the course catalog.  Violations
+ will be dealt with as outlined therein.
+ 
+ All programming assignments in this class are to be done by the
+ student alone.  No outside help is permitted except the instructor
+ and approved tutors.
+ 
+ I certify that the work submitted with this assignment is mine and
+ was generated in a manner consistent with this document, the
+ course academic policy on the course website on Blackboard, and
+ the UMass Lowell academic code.
+ 
+ Date: 01/30/2021
+ Name: Aaron Kennen
+ */
+ 
+ /*
+  Author    : Aaron Kennen
+  Date      : 01/30/2021
+  Output    : This program has the executable name: ./a.out
+              That is all!
+ */
+ /*
+ #include <stdio.h>
+ 
+ int main(int argc, char * argv[]){
+    printf("This program has the executable name: %s\n", argv[0]);
+    printf("That is all!\n");
+    return 0;
+ }
+*/
