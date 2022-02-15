6d8dad8f66b1bc07a4fd6c733f19e15f18f8a7f4
Aaron Kennen <79381363+aaronkennen@users.noreply.github.com>
2022-02-15 11:25:45 -0500

    Create Daily01.c

---
+++ Daily01.c
@@ -0,0 +1,50 @@
+/*
+ Computing I -- COMP.1010 Honor Statement
+ 
+ The practice of good ethical behavior is essential for maintaining
+ good order in the classroom, providing an enriching learning
+ experience for students, and as training as a practicing computing
+ professional upon graduation.  This practice is manifested in the
+ University's Academic Integrity policy. Students are expected to
+ strictly avoid academic dishonesty and adhere to the Academic
+ Integrity policy as outlined in the course catalog.  Violations
+ will be dealt with as outlined therein.
+ 
+ All programming assignments in this class are to be done by the
+ student alone.  No outside help is permitted except the instructor
+ and approved tutors.
+ 
+ I certify that the work submitted with this assignment is mine and
+ was generated in a manner consistent with this document, the
+ course academic policy on the course website on Blackboard, and
+ the UMass Lowell academic code.
+ 
+ Date: 1/28/2021
+ Name: Aaron Kennen
+ */
+
+/***********************************************
+ Author: Aaron Kennen
+ Date: 1/28/2021
+ Purpose: to become familiar with zyLab to finish programming assignments
+ Sources of Help: None, just my brain
+ 
+ Time Spent: about 10 minutes
+ 
+ ***********************************************/
+/*
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+    int userNum;
+    int userNumSquared;
+    
+    scanf("%d", &userNum);
+    
+    userNumSquared = userNum * userNum;     // Bug here; fix it when instructed
+                                            //-- Fixed by changing + for *
+    
+    printf("%d\n", userNumSquared);         // Output formatting issue here; fix it when instructed
+                                            //-- Fixed by putting \n after the %d
+    return 0;
+}
+*/
